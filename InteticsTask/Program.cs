cb23cca0a9beb8e18d78a47d02a3c1f290aebcb0
Basem <basem.kamal@itworx.xom>
2022-04-16 05:42:07 +0200

    Add project files.

---
+++ InteticsTask/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace InteticsTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bufferToPrint = new char[512];
+            int bufferToPrintCount = 0;
+            var bufferToRead = new char[512];
+            int bufferToReadCount;
+            int charsToCopyLength;
+            do
+            {
+                bufferToReadCount = GetData(out bufferToRead);
+                if (bufferToReadCount != 0)
+                {
+                    charsToCopyLength = Math.Min(bufferToReadCount, bufferToPrint.Length - bufferToPrintCount);
+                    Array.Copy(bufferToRead, 0, bufferToPrint, bufferToPrintCount, charsToCopyLength);
+                    bufferToPrintCount += charsToCopyLength;
+                    if (bufferToPrintCount == bufferToPrint.Length)
+                    {
+                        PutData(bufferToPrint, bufferToPrintCount);
+                        bufferToPrintCount = 0;
+                        if (bufferToReadCount > charsToCopyLength)
+                        {
+                            Array.Copy(bufferToRead, charsToCopyLength, bufferToPrint, 0, bufferToReadCount - charsToCopyLength);
+                            bufferToPrintCount = bufferToReadCount - charsToCopyLength;
+                        }
+                    }
+                }
+            }
+            while (bufferToReadCount != 0);
+            if (bufferToPrintCount != 0)
+            {
+                PutData(bufferToPrint, bufferToPrintCount);
+            }
+        }
+
+        private static int GetData(out char[] bufferToRead)
+        {
+            bufferToRead = new char[512];
+            return 512;
+        }
+
+        private static void PutData(char[] bufferToPrint, int count)
+        {
+            Console.WriteLine("buffer To Print");
+        }
+    }
+}
